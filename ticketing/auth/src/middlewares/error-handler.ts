f2a0815881213c86931f6f22118b93282b466a21
tangyisheng2 <tangyisheng2@sina.com>
2022-06-09 16:19:16 -0700

    fix: <auth> Cannot set headers after they are sent to the client

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -9,15 +9,20 @@ export const errorHandler = (
     next: NextFunction
 ) => {
     if (err instanceof RequestValidationError) {
-        const formattedErrors = err.errors.map((error) => ({
-            message: error.msg,
-            field: error.param,
-        }));
+        const formattedErrors = err.errors.map((error) => {
+            // Use return to fix: Error: Cannot set headers after they are sent to the client
+            return {
+                message: error.msg,
+                field: error.param,
+            };
+        });
         res.status(400).send(formattedErrors);
+        return;
     }
 
     if (err instanceof DatabaseConnectionError) {
         console.log('Handling DB Error');
+        return;
     }
 
     res.status(400).send({
