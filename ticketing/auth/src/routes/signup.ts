a989feb1d9972c1fad24dbd41a81d1590a4db260
tangyisheng2 <tangyisheng2@sina.com>
2022-06-09 16:59:31 -0700

    chore: <auth> optimize file structure

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../middlewares/database-connection-error';
-import { RequestValidationError } from '../middlewares/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
