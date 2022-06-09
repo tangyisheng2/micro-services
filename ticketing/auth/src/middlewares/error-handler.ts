e7aa91d174b8cbc9be07ebb429a284a07170aab1
tangyisheng2 <tangyisheng2@sina.com>
2022-06-09 14:38:11 -0700

    feat: <auth> add middleware error-handler

---
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    console.log('Error: ', err);
+    res.status(400).send({
+        msg: err,
+    });
+};
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -16,10 +16,11 @@ router.post(
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
+            throw new Error('Invalid email or password'); // express会将error自动提交给中间件
         }
 
         const { email, password } = req.body;
+        throw new Error('Error connecting to database');
 
         res.send({ email, password });
     }
