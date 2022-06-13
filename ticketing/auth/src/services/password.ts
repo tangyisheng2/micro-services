a4fd46280627345bf473680c234725d3f9c42ca0
tangyisheng2 <tangyisheng2@sina.com>
2022-06-13 16:41:54 -0700

    feat: <auth> add password hashing

---
+++ ticketing/auth/src/services/password.ts
@@ -0,0 +1,22 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+    static async toHash(password: string) {
+        const salt = randomBytes(8).toString('hex');
+
+        const res = await scryptAsync(password, salt, 64);
+        const buf = res as Buffer; // scryptAsync normally returns unknown, convert to Buffer type
+        return `${buf.toString('hex')}.${salt}`;
+    }
+
+    static compare(storedPassword: string, suppliedPassword: string) {
+        const [hashedPassword, salt] = storedPassword.split('.');
+        scryptAsync(suppliedPassword, salt, 64).then((res) => {
+            const buf = res as Buffer; // scryptAsync normally returns unknown, convert to Buffer type
+            return buf.toString('hex') === hashedPassword;
+        });
+    }
+}
