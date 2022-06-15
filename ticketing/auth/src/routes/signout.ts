4881390b42f61abc9f885e162190732f4a1c23ae
tangyisheng2 <tangyisheng2@sina.com>
2022-06-15 16:24:56 -0700

    feat: <auth> add signout logic

--- ticketing/auth/src/routes/signout.ts
+++ ticketing/auth/src/routes/signout.ts
@@ -5,7 +5,9 @@ const router = express.Router();
  * This function signs out a user by clearing all the cookies
  */
 router.post('/api/users/signout', (req, res) => {
-    res.send('sign out!');
+    req.session = null;
+
+    res.send({});
 });
 
 export { router as signoutRouter };
