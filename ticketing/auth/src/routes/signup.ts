3ed8b90e54ba0b2ce41f099f6df218782ea5ad3a
tangyisheng2 <tangyisheng2@sina.com>
2022-06-14 20:31:17 -0700

    feat: <auth> add JWT support when sign up

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -51,6 +52,20 @@ router.post(
 
         const user = User.build({ email, password });
         await user.save();
+        // Generate JWT
+        const userJwt = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+            },
+            '1234'
+        );
+
+        // Store it in session object
+        req.session = {
+            jwt: userJwt,
+        };
+
         res.status(201).send(user);
     }
 );
