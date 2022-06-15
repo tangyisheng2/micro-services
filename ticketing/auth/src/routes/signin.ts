6e3107da414c20f50b5bc34532c4f1d584da6222
tangyisheng2 <tangyisheng2@sina.com>
2022-06-15 15:53:24 -0700

    feat: <auth> add signin logic

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -14,7 +18,39 @@ router.post(
             .withMessage('You must supply a password'),
     ],
     validateRequest,
-    (req: Request, res: Response) => {}
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            throw new BadRequestError('Invalid Credentials');
+        }
+
+        const passwordMatch = await Password.compare(
+            existingUser.password,
+            password
+        );
+
+        if (!passwordMatch) {
+            throw new BadRequestError('Invalid Credentials');
+        }
+
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email,
+            },
+            process.env.JWT_KEY!
+        );
+
+        // Store it in session object
+        req.session = {
+            jwt: userJwt,
+        };
+
+        res.status(200).send(existingUser);
+    }
 );
 
 export { router as signinRouter };
