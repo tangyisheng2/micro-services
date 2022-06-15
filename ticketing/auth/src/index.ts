ecfba6bcfe1178d69dce066b2766934c0f1b1cfd
tangyisheng2 <tangyisheng2@sina.com>
2022-06-14 20:24:21 -0700

    feat: <auth> add cookie-session capability

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -9,14 +9,22 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true); // Traffic is proxyed to express, and trust the traffic
 app.use(json());
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true, // Require HTTPS connection
+    })
+);
 
 app.all('*', () => {
     throw new NotFoundError();
