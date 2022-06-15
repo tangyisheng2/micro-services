ec92d6ea7a2cb959c85b7a1dedb2066a83dcebfd
tangyisheng2 <tangyisheng2@sina.com>
2022-06-15 16:52:52 -0700

    feat: <auth> add CurrentUser middleware

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,24 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 /**
  * This function gets the info of the current signed user
  * by looking up the JWT in the cookies
  */
-router.get('/api/users/currentuser', (req, res) => {
-    if (!req.session?.jwt) {
-        // "?" checks if the internal property exist or not
-        return res.send({ currentUser: null });
-    }
-
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        // JWT will throw an error is verify failed
-        res.send({ currentUser: payload });
-    } catch (error) {
-        return res.send({ currentUser: null });
-    }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
