e21c07f1d228bfa4d6654e0f0102ca2b8991d47d
tangyisheng2 <tangyisheng2@sina.com>
2022-06-15 16:10:30 -0700

    feat: <auth> add currentUser logic

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+    if (!req.session?.jwt) {
+        // "?" checks if the internal property exist or not
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        // JWT will throw an error is verify failed
+        res.send({ currentUser: payload });
+    } catch (error) {
+        return res.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter };
