ef9994faee2bf4da18957115932b677a1b6e1c83
tangyisheng2 <tangyisheng2@sina.com>
2022-06-15 17:00:04 -0700

    feat: <auth> add requireAuth middleware

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 /**
  * This function gets the info of the current signed user
  * by looking up the JWT in the cookies
  */
-router.get('/api/users/currentuser', currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
     res.send({ currentUser: req.currentUser || null });
 });
 
