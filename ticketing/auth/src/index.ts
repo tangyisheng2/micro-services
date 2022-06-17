47ca99a7e460be423834e92612deaf6004cae8e4
tangyisheng2 <tangyisheng2@sina.com>
2022-06-16 17:27:35 -0700

    feat: <auth> refactor the app for test lib

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,42 +1,7 @@
-import express from 'express';
 import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true); // Traffic is proxyed to express, and trust the traffic
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false, // Disable encryption
-        // secure: true, // Require HTTPS connection
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', () => {
-    throw new NotFoundError();
-});
-
-// Async version
-// app.all('*', async (req, res, next) => {
-//     next( new NotFoundError());
-// });
-// Or use express-async-errors
-
-app.use(errorHandler);
+import { app } from './app';
 
 if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
