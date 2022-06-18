8d629e1573a5278e32c20f397da1fb550228957b
tangyisheng2 <tangyisheng2@sina.com>
2022-06-17 20:36:58 -0700

    feat: <client> update signup page

--- ticketing/client/pages/auth/signup.jsx
+++ ticketing/client/pages/auth/signup.jsx
@@ -1,18 +1,39 @@
+import { useState } from 'react';
+
 function signUp() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitData = (e) => {
+        e.preventDefault();
+
+        console.log({ email, password });
+    };
+
     return (
-        <form>
+        <form onSubmit={submitData}>
             <h1>Sign Up Form</h1>
             <div>
                 <div className="form-group">
                     <label>
                         Email Address
-                        <input type="text" className="form-control" />
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="text"
+                            className="form-control"
+                        />
                     </label>
                 </div>
                 <div className="form-group">
                     <label>
                         Password
-                        <input type="password" className="form-control" />
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            className="form-control"
+                        />
                     </label>
                 </div>
                 <button className="btn btn-primary">signUp</button>
