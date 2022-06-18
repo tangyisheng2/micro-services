c135753713305543988775569fe3073e3998423a
tangyisheng2 <tangyisheng2@sina.com>
2022-06-17 22:39:58 -0700

    feat: <auth> update signup page

--- ticketing/client/pages/auth/signup.jsx
+++ ticketing/client/pages/auth/signup.jsx
@@ -4,21 +4,53 @@ import { useState } from 'react';
 function signUp() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [successMsg, setSuccessMsg] = useState({});
+    const [errors, setErrors] = useState([]);
 
     const submitData = async (e) => {
         e.preventDefault();
 
-        const response = await axios.post('/api/users/signup', {
-            email,
-            password,
-        });
-
-        console.log(response.data);
+        axios
+            .post('/api/users/signup', {
+                email,
+                password,
+            })
+            .then((result) => {
+                console.log(result);
+                setSuccessMsg(result.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                setErrors(err.response.data);
+            });
     };
 
     return (
         <form onSubmit={submitData}>
             <h1>Sign Up Form</h1>
+            {errors.length > 0 && (
+                <div className="alert alert-danger">
+                    <ul>
+                        {errors.map((err) => {
+                            return <li key={err.message}>{err.message}</li>;
+                        })}
+                    </ul>
+                </div>
+            )}
+            {successMsg && (
+                <div className="successMsg success">
+                    <ul>
+                        {successMsg &&
+                            Object.keys(successMsg).map((entry) => {
+                                return (
+                                    <li
+                                        key={entry}
+                                    >{`${entry}: ${successMsg[entry]}`}</li>
+                                );
+                            })}
+                    </ul>
+                </div>
+            )}
             <div>
                 <div className="form-group">
                     <label>
