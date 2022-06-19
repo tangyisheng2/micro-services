52d34ceee5cbbb26b37420881c7e4ac150ba3ba1
tangyisheng2 <tangyisheng2@sina.com>
2022-06-19 19:14:28 +0000

    feat: <client> update index page showing currentUser

--- ticketing/client/pages/index.jsx
+++ ticketing/client/pages/index.jsx
@@ -1,4 +1,38 @@
-function Index() {
-    return <h1>Hi</h1>;
+import axios from 'axios';
+
+function Index({ currentUser }) {
+    console.log(currentUser);
+    return (
+        <>
+            <h1>Home Page</h1>
+            <p>Current User: {currentUser}</p>
+        </>
+    );
 }
+
+Index.getInitialProps = () => {
+    if (typeof window === 'undefined') {
+        console.log('On server!');
+        // Request should be made to 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+    } else {
+        console.log('On browser');
+        // Request can be make to empty base url
+    }
+
+    return axios
+        .get(
+            // Request Option #1
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser'
+        )
+        .then((res) => {
+            const response = res.data;
+            console.log(response);
+            return response;
+        })
+        .catch((err) => {
+            console.log(err);
+            return { currentUser: null };
+        });
+};
+
 export default Index;
