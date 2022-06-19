f00a7ae230441f80e0f8582dcc4ef46532f80d69
tangyisheng2 <tangyisheng2@sina.com>
2022-06-19 21:55:28 +0000

    feat: <client> update index page logic for getInitialProps running in the browser and the node

--- ticketing/client/pages/index.jsx
+++ ticketing/client/pages/index.jsx
@@ -11,27 +11,34 @@ function Index({ currentUser }) {
 }
 
 Index.getInitialProps = () => {
+    let url = '/api/users/currentuser';
     if (typeof window === 'undefined') {
         console.log('On server!');
+        url =
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
         // Request should be made to 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
     } else {
         console.log('On browser');
+        url = '/api/users/currentuser';
         // Request can be make to empty base url
     }
 
     return axios
         .get(
             // Request Option #1
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser'
+            url,
+            {
+                host: 'ticketing.dev',
+            }
         )
         .then((res) => {
             const response = res.data;
             console.log(response);
-            return response;
+            return { currentUser: response.currentUser.email };
         })
         .catch((err) => {
-            console.log(err);
-            return { currentUser: null };
+            // console.log(err);
+            return { currentUser: 'Not Authorized' };
         });
 };
 
