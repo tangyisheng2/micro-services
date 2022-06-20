09a1099983a47b6107171b59e017abf530a0c21a
tangyisheng2 <tangyisheng2@sina.com>
2022-06-20 19:07:53 +0000

    feat: <client> add header in _app component

--- ticketing/client/pages/_app.jsx
+++ ticketing/client/pages/_app.jsx
@@ -1,6 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
-function _app({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+import BuildClient from '../api/build-client';
+/**
+ * The custom app component
+ * Note that the parameter passed in are different
+ * from the page component
+ * Page component：context === {req, res}
+ * Custom App Component: context === {Component, ctx: {req, res}}
+ * @param {Object} param0 The request component
+ * @returns React Object
+ */
+function _app({ Component, pageProps, currentUser }) {
+    return (
+        <div>
+            <h1>Header {currentUser.email}</h1>
+            <Component {...pageProps} />
+        </div>
+    );
 }
+
+/**
+ * This method fetch the data when the component first renders
+ * Note: If you chained components, you need to manually run
+ * the .getInitialProps() method in the nested component and
+ * pass the result to the nested component
+ * @param {Object} appContext The request Object from next.js
+ * @returns Fetched data
+ */
+_app.getInitialProps = async (appContext) => {
+    const client = BuildClient(appContext.ctx);
+    // console.log(appContext.ctx);
+
+    const data = (await client.get('/api/users/currentuser')).data;
+    const pageProps = await appContext.Component.getInitialProps(
+        appContext.ctx
+    );
+    return {
+        pageProps,
+        ...data,
+    };
+};
+
 export default _app;
