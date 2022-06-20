ce4c1c0c0dcb1c92b4c4c3caf07ab7b1798a16df
tangyisheng2 <tangyisheng2@sina.com>
2022-06-20 06:20:11 +0000

    feat: <client> add signin page

---
+++ ticketing/client/pages/auth/signin.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+
+function signIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [successMsg, setSuccessMsg] = useState({});
+    // const [errors, setErrors] = useState([]);
+    console.log({ email, password });
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signin',
+        method: 'post',
+        body: {
+            email,
+            password,
+        },
+        onSuccess: () => {
+            Router.push('/');
+        },
+    });
+
+    const submitData = async (e) => {
+        e.preventDefault();
+
+        doRequest();
+
+        // axios
+        //     .post('/api/users/signup', {
+        //         email,
+        //         password,
+        //     })
+        //     .then((result) => {
+        //         console.log(result);
+        //         setSuccessMsg(result.data);
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //         setErrors(err.response.data);
+        //     });
+    };
+
+    return (
+        <form onSubmit={submitData}>
+            <h1>Sign Up Form</h1>
+            {errors}
+            {successMsg && (
+                <div className="successMsg success">
+                    <ul>
+                        {successMsg &&
+                            Object.keys(successMsg).map((entry) => {
+                                return (
+                                    <li
+                                        key={entry}
+                                    >{`${entry}: ${successMsg[entry]}`}</li>
+                                );
+                            })}
+                    </ul>
+                </div>
+            )}
+            <div>
+                <div className="form-group">
+                    <label>
+                        Email Address
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="text"
+                            className="form-control"
+                        />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                        Password
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            className="form-control"
+                        />
+                    </label>
+                </div>
+                <button className="btn btn-primary">signIn</button>
+            </div>
+        </form>
+    );
+}
+export default signIn;
