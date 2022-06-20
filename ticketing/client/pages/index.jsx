f8d7ffa58d2c67f4b1760172f2bf38cd5f08910c
tangyisheng2 <tangyisheng2@sina.com>
2022-06-20 19:07:26 +0000

    chore: <client> refactor code on index

--- ticketing/client/pages/index.jsx
+++ ticketing/client/pages/index.jsx
@@ -1,7 +1,11 @@
 import BuildClient from '../api/build-client';
 
+/**
+ * This is the component of the index
+ * @param {Object} param0 currentUser: The info of current loged in user
+ * @returns React Object
+ */
 function Index({ currentUser }) {
-    console.log(currentUser);
     return (
         <>
             <h1>Home Page</h1>
@@ -10,13 +14,17 @@ function Index({ currentUser }) {
     );
 }
 
+/**
+ * This method fetch the data when the component first renders
+ * @param {Object} param0 Request object that has the req field
+ * @returns Fetched currentUser data
+ */
 Index.getInitialProps = ({ req }) => {
     const axiosClient = BuildClient({ req });
     return axiosClient
         .get('/api/users/currentuser')
         .then((res) => {
             const response = res.data;
-            console.log(response);
             return { currentUser: response.currentUser.email };
         })
         .catch((err) => {
