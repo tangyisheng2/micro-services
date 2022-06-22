9a5c34b99dfbb4ba3aa727eee3d364d9454febbd
tangyisheng2 <tangyisheng2@sina.com>
2022-06-22 17:22:23 +0000

    feat: <tickets> refactor mongo_uri to deployment config

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -7,8 +7,12 @@ if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
 }
 
+if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+}
+
 mongoose
-    .connect('mongodb://tickets-mongo-srv:27017/tickets')
+    .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(3000, () => {
             console.log('Listening on port 3000');
