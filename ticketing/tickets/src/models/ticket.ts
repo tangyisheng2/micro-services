7da2bd3e008e96f6a0daddc7c46a9080b2c138e8
tangyisheng2 <tangyisheng2@sina.com>
2022-06-23 04:06:41 +0000

    feat: <tickets> add ticket schema

---
+++ ticketing/tickets/src/models/ticket.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+interface TicketAttrs {
+    title: string;
+    price: number;
+    userId: string;
+}
+
+interface TicketDoc extends mongoose.Document {
+    title: string;
+    price: number;
+    userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+    build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        userId: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            /**
+             * Functions to manipulate the JSON representation of the object
+             * @param doc
+             * @param ret The modified user object
+             */
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+    return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
