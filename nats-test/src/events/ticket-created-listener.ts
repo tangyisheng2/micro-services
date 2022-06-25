409df3d048a43a468f43ba925de1bbe20a1d363c
tangyisheng2 <tangyisheng2@sina.com>
2022-06-25 22:26:04 +0000

    fix: <nats-test> fix missing subject type definition

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -6,7 +6,8 @@ import { Subjects } from './subjects';
 class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     // Need to provide a type annotation to make sure that we are not
     // Changing the value in the future
-    readonly subject = Subjects.TicketCreated;
+    // Or we can use readonly modifier
+    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName: string = 'payments-service';
 
     onMessage(data: TicketCreatedEvent['data'], message: Message): void {
