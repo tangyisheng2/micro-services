43a5056790aeeab511a9e68f6d25082148ec96e9
tangyisheng2 <tangyisheng2@sina.com>
2022-06-25 22:10:31 +0000

    feat: <nats-test> add publisher events

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -9,29 +10,36 @@ const stan = nats.connect('ticketing', 'abc', {
 /**
  * On connect
  */
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
 
-    const data = JSON.stringify({
+    const publisher = new TicketCreatedPublisher(stan);
+    await publisher.publish({
         id: '123',
-        time: new Date().toLocaleDateString(),
+        title: 'concert',
         price: 20,
     });
 
-    stan.on('close', () => {
-        console.log('NATS Connection closed!');
-        process.exit();
-    });
+    // const data = JSON.stringify({
+    //     id: '123',
+    //     time: new Date().toLocaleDateString(),
+    //     price: 20,
+    // });
 
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published', data);
-    });
+    // stan.publish('ticket:created', data, () => {
+    //     console.log('Event published', data);
+    // });
 });
 /**
  * On disconnect
  */
 stan.on('disconnect', () => {
     console.log('Publisher disconnected to NATS');
+});
+
+stan.on('close', () => {
+    console.log('NATS Connection closed!');
+    process.exit();
 });
 
 process.on('SIGTERM', () => stan.close());
