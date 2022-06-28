100521fe5145fa51f43921d9ea6da0583dfd5ff9
tangyisheng2 <tangyisheng2@sina.com>
2022-06-28 00:30:24 +0000

    feat: <orders> initial commit for orders

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,43 @@
+import { json } from 'body-parser';
+import express from 'express';
+import 'express-async-errors';
+
+import {
+    currentUser,
+    errorHandler,
+    NotFoundError,
+} from '@tangyisheng2-ticket/common';
+import cookieSession from 'cookie-session';
+import { indexTicketRouter } from './routes';
+import { createTicketRouter } from './routes/new';
+import { showTicketsRouter } from './routes/showTicket';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true); // Traffic is proxyed to express, and trust the traffic
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false, // Disable encryption
+        // secure: true, // Require HTTPS connection
+    })
+);
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketsRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', () => {
+    throw new NotFoundError();
+});
+
+// Async version
+// app.all('*', async (req, res, next) => {
+//     next( new NotFoundError());
+// });
+// Or use express-async-errors
+
+app.use(errorHandler);
+
+export { app };
