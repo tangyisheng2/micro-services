606a889e844d1df0055122c3ece4da65767b2f13
tangyisheng2 <tangyisheng2@sina.com>
2022-07-10 23:36:50 +0000

    feat: <common> publisher will log the content out

--- ticketing/common/src/events/base-publisher.ts
+++ ticketing/common/src/events/base-publisher.ts
@@ -15,6 +15,10 @@ export abstract class Publisher<T extends Event> {
     }
 
     publish(data: T['data']): Promise<void> {
+        console.log(
+            `Message published: ${this.subject}
+            : ${data}`
+        );
         return new Promise((resolve, reject) => {
             this.client.publish(this.subject, JSON.stringify(data), (err) => {
                 if (err) {
